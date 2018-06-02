2f443ec4d852ff9f6b61032106f101347b3644ea
Victor <x89168419357@gmail.com>
2018-06-02 15:08:10 +0300

    utils - indents fix, color functions refactored, ...

--- src/utils.js
+++ src/utils.js
@@ -1,81 +1,82 @@
 function rotateArray(array, rotate) {
-    rotate -= array.length * Math.floor(rotate / array.length);
-    array.push(...array.splice(0, rotate));
+  rotate -= array.length * Math.floor(rotate / array.length);
+  array.push(...array.splice(0, rotate));
 }
 
 function limitation(val, min, max) {
-    return Math.min(max, Math.max(min, val >> 0));
+  return Math.min(max, Math.max(min, val | 0));
 }
 
 function bitMask(size) {
-    return Math.pow(2, size) - 1;
+  return Math.pow(2, size) - 1;
 }
 
-function getColorComponents(color) {
-    return [ 1, 3, 5 ].map(n => parseInt(color.slice(n, n + 2), 16));
+function getColorComponents(color, str) {
+  return color.match(/(\w\w)+?/g).map(n => str ? n : parseInt(n, 16));
 }
 
 function transformColor(color) {
-    return (parseInt(color.slice(5) + color.slice(3, 5) + color.slice(1, 3), 16)) | (0xFF << 24);
+  return parseInt(getColorComponents(color, true).reverse().join(''), 16) | (0xFF << 24);
 }
 
 function gradient(from, to, numSteps) {
-    var componentsFrom = getColorComponents(from),
-        componentsTo = getColorComponents(to),
-        componentsItems = componentsFrom.map((n, i) => (n - componentsTo[i]) / (numSteps - 1));
+  const
+    componentsFrom = getColorComponents(from),
+    componentsTo = getColorComponents(to),
+    componentsItems = componentsFrom.map((n, i) => (n - componentsTo[i]) / (numSteps - 1));
 
-    return [...Array(numSteps)].map((n, i) => {
-        return componentsFrom.map((m, j) => ((m - i * componentsItems[j]) | 0).toString(16).padStart(2, '0')).join('');
-    });
+  return [...Array(numSteps)].map((n, i) => {
+    return componentsFrom.map((m, j) => ((m - i * componentsItems[j]) | 0).toString(16).padStart(2, '0')).join('');
+  });
 }
 
 function getLineCoord(p0, p1) {
-    var x = p0.x,
-        y = p0.y,
-        dx = Math.abs(x - p1.x),
-        dy = Math.abs(y - p1.y),
-        sx = (x < p1.x) ? 1 : -1,
-        sy = (y < p1.y) ? 1 : -1,
-        error = dx - dy,
-        coord = [];
+  let
+    { x, y } = p0,
+    dx = Math.abs(x - p1.x),
+    dy = Math.abs(y - p1.y),
+    sx = (x < p1.x) ? 1 : -1,
+    sy = (y < p1.y) ? 1 : -1,
+    error = dx - dy,
+    coord = [];
 
-    while (true) {
-        coord.push({ x, y });
+  while (true) {
+    coord.push({ x, y });
 
-        if ((x === p1.x) && (y === p1.y)) {
-            break;
-        }
-
-        var e2 = error * 2;
-        if (e2 > -dy) {
-            error -= dy;
-            x += sx;
-        }
-        if (e2 < dx) {
-            error += dx;
-            y += sy;
-        }
+    if ((x === p1.x) && (y === p1.y)) {
+      break;
     }
 
-    return coord;
+    const e2 = error * 2;
+    if (e2 > -dy) {
+      error -= dy;
+      x += sx;
+    }
+    if (e2 < dx) {
+      error += dx;
+      y += sy;
+    }
+  }
+
+  return coord;
 }
 
 function logExecutionTime(message) {
-    return function(target, key, descriptor) {
-        const original = descriptor.value;
+  return function(target, key, descriptor) {
+    const original = descriptor.value;
 
-        descriptor.value = function(...args) {
-            const
-              start = performance.now(),
-              result = original.apply(this, args);
+    descriptor.value = function(...args) {
+      const
+        start = performance.now(),
+        result = original.apply(this, args);
 
-            console.log(`${message}: ${performance.now() - start}`);
+      console.log(`${message}: ${performance.now() - start}`);
 
-            return result;
-        };
-
-        return descriptor;
+      return result;
     };
+
+    return descriptor;
+  };
 }
 
 export { rotateArray, limitation, bitMask, getColorComponents, transformColor, gradient, getLineCoord, logExecutionTime };
