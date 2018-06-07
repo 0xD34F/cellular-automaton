34b6f19e7d06b9f2162e183bf12be76588581456
Victor <x89168419357@gmail.com>
2018-06-07 15:24:11 +0300

    element-ui added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 ﻿import Vue from 'vue';
+import Element from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+
+Vue.use(Element);
+
 
 new Vue({
   el: '#app',
